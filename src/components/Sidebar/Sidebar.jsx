12499a8e34d70620260f5ed587b308b783db2cee
mariaemf <mariaeduardamartinsf@gmail.com>
2023-01-02 16:40:43 -0300

    change and incrementation inside the sidebar, aiming to be stylizing it and adding content inside

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,18 +1,44 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import navLinks from "../../assets/dummy-data/navLinks";
+import "./sidebar.css";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar__top">
         <h2>
-          <i class="ri-taxi-line">UberX</i>
+          <span>
+            <i class="ri-taxi-line"></i>
+          </span>{" "}
+          UberX
         </h2>
       </div>
+
       <div className="sidebar__content">
         <div className="menu">
           <ul className="nav__list">
-            <lii className="nav__item"></lii>
+            {navLinks.map((item, index) => (
+              <li className="nav__item" key={index}>
+                <NavLink
+                  to={item.path}
+                  className={(navClass) =>
+                    navClass.isActive ? "nav__active nav__link" : "nav__link"
+                  }
+                >
+                  <i className={item.icon}></i>
+
+                  {item.display}
+                </NavLink>
+              </li>
+            ))}
           </ul>
+        </div>
+
+        <div className="sidebar__bottom">
+          <span>
+            <i class="ri-logout-circle-r-line">Logout</i>
+          </span>
         </div>
       </div>
     </div>
@@ -20,3 +46,5 @@ const Sidebar = () => {
 };
 
 export default Sidebar;
+
+/*a key retorna um array no qual os elementos sao strings correspondentes para a propriedade encontrada diretamente sobre o objeto*/
--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
+
 import Bookings from "../pages/Bookings";
 import Dashboard from "../pages/Dashboard";
 import SellCar from "../pages/SellCar";
@@ -7,19 +8,16 @@ import Settings from "../pages/Settings";
 
 const Router = () => {
   return (
-    //BrowserRouter é im componente responsável por informar a aplicação que teremos um roteamento de componentes por isso ele ficará por volta dos componentes <Route>
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="./"
-          element={<Navigate to="/dashboard" element={<Dashboard />} />}
-        />
-        <Route path="./dashboard" element={<Dashboard />} />
-        <Route path="./bookings" element={<Bookings />} />
-        <Route path="./sellCar" element={<SellCar />} />
-        <Route path="./settings" element={<Settings />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route
+        path="/"
+        element={<Navigate to="/dashboard" element={<Dashboard />} />}
+      />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/bookings" element={<Bookings />} />
+      <Route path="/sell-car" element={<SellCar />} />
+      <Route path="/settings" element={<Settings />} />
+    </Routes>
   );
 };
 

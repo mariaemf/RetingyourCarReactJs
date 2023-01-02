e441f15da2a52b25b004731a55638d970a8e9c02
mariaemf <mariaeduardamartinsf@gmail.com>
2023-01-02 18:09:42 -0300

    performing implementations within the top-nav session and also adding your initial stylizations

--- src/components/TopNav/TopNav.jsx
+++ src/components/TopNav/TopNav.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import profileImg from "../../assets/images/profile-02.png";
+import "./top-nav.css";
 
 const TopNav = () => {
-  return <div>TopNav</div>;
+  return (
+    <div className="top__nav">
+      <div className="top__nav-wrapper">
+        <div className="search__box">
+          <input type="text" placeholder="search ot type" />
+          <span>
+            <i class="ri-search-line"></i>
+          </span>
+        </div>
+        <div className="top__nav-right">
+          <span className="notification">
+            <i class="ri-notification-3-line"></i>
+          </span>
+          <div className="profile">
+            <Link to="./settings">
+              <img src={profileImg} alt="" />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TopNav;
+
+/*
+placeholder = pequena dease que tem o intuito de ajudar o usuário a ter noção do que preencher no campo*/

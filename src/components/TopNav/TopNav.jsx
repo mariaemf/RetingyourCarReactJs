8265c6a7750aaa7af52b275bdc0a1dab2c1d36d2
mariaemf <mariaeduardamartinsf@gmail.com>
2023-01-02 22:30:53 -0300

    top-nav finishes and stylizations

--- src/components/TopNav/TopNav.jsx
+++ src/components/TopNav/TopNav.jsx
@@ -16,6 +16,7 @@ const TopNav = () => {
         <div className="top__nav-right">
           <span className="notification">
             <i class="ri-notification-3-line"></i>
+            <span className="badge">1</span>
           </span>
           <div className="profile">
             <Link to="./settings">

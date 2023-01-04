7c08e19b7ffb2ba572f6fb5a42eb672522fa97d9
mariaemf <mariaeduardamartinsf@gmail.com>
2023-01-03 22:14:42 -0300

    implementing stylizations and adding remix.icon items

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -50,6 +50,42 @@ const Dashboard = () => {
             <CarStatsChart />
           </div>
         </div>
+
+        <div className="recommend__cars-wrapper">
+          <div className="recommend__car-card">
+            <div className="recommend__car-top">
+              <h5>
+                <span>
+                  <i class="ri-restart-line"></i>
+                </span>
+                74% Recommended
+              </h5>
+            </div>
+
+            <div className="recommend__car-img">
+              <img src="../assets/images/mini-car-01.png" alt="" />
+            </div>
+            <div className="recommend__car-bottom">
+              <h4>Mini Cooper</h4>
+              <div className="recommend__car-other">
+                <div>
+                  <p>
+                    <span>
+                      <i class="ri-repeat-line"></i>
+                    </span>{" "}
+                    130k
+                  </p>
+                  <p>
+                    <span>
+                      <i class="ri-timer-flash-line"></i>
+                    </span>
+                  </p>
+                </div>
+                <span>$30/h</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );

c261253c67893a8881246abd5653b615e2bad15e
mariaemf <mariaeduardamartinsf@gmail.com>
2023-01-05 18:57:57 -0300

    CarItem session realization, in-page incrementation of jsx and css booking

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import carImg from "../assets/images/car-01.png";
+import carData from "../assets/dummy-data/booking-cars";
+import CarItem from "../components/UI/CarItem";
 import "../styles/booking.css";
 
 const Bookings = () => {
@@ -13,7 +14,7 @@ const Bookings = () => {
             <select>
               <option value="new">New</option>
               <option value="popular">Popular</option>
-              <option value="upcoming"></option>
+              <option value="upcoming">Upcoming</option>
             </select>
           </div>
 
@@ -26,32 +27,9 @@ const Bookings = () => {
           </div>
         </div>
         <div className="booking__car-list">
-          <div className="car__item">
-            <div className="car__item-top">
-              <div className="car__item-tile">
-                <h3>Nissan</h3>
-                <span>
-                  <i class="ri-heart-line"></i>
-                </span>
-              </div>
-              <p>category</p>
-            </div>
-            <div className="car__img">
-              <img src={carImg} alt="" />
-            </div>
-
-            <div className="car__item-bottom">
-              <div className="car__bottom-left">
-                <p>
-                  <i class="ri-user-line"></i>4
-                </p>
-                <p>
-                  <i class="ri-repeat-line"></i>Manual
-                </p>
-              </div>
-              <p>$29/h</p>
-            </div>
-          </div>
+          {carData?.map((item) => (
+            <CarItem item={item} key={item.id} />
+          ))}
         </div>
       </div>
     </div>

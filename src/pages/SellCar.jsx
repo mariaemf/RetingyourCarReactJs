24ca896d8c760a61aa3f45bf48295f8b60d4358e
mariaemf <mariaeduardamartinsf@gmail.com>
2023-01-09 10:33:11 -0300

    sellcar session completion (jsx and css)

--- src/pages/SellCar.jsx
+++ src/pages/SellCar.jsx
@@ -6,6 +6,7 @@ import "../styles/sellcar.css";
 
 const SellCar = () => {
   const percentage = 55;
+  const percentage02 = 45;
 
   return (
     <div className="sell__car">
@@ -56,6 +57,61 @@ const SellCar = () => {
                   <CircularProgressbar
                     value={percentage}
                     text={`${percentage}%`}
+                    styles={buildStyles({
+                      pathColor: "#01d293",
+                      textColor: "#fff",
+                      trailColor: "#0b0c28",
+                      textSize: "18px",
+                    })}
+                  />
+                </div>
+                <h4>Impression Share</h4>
+              </div>
+
+              <div className="box__03">
+                <span className="model__spend-icon">
+                  <i class="ri-car-line"></i>
+                </span>
+                <h6 className="spend__amount">$1174</h6>
+                <p className="spend__title">Model Spend</p>
+              </div>
+
+              <div className="box__04">
+                <span className="model__spend-icon">
+                  <i class="ri-share-forward-line"></i>
+                </span>
+                <h6 className="spend__amount">$1174</h6>
+                <p className="spend__title">Model Spend</p>
+              </div>
+
+              <div className="box__05">
+                <span className="model__spend-icon">
+                  <i class="ri-money-dollar-circle-line"></i>
+                </span>
+                <h6 className="spend__amount">$811</h6>
+                <p className="spend__title">Spend Per Unit Turned</p>
+              </div>
+            </div>
+
+            <div className="offer__item">
+              <div className="box__01">
+                <h3 className="client__name">Jhon Doe </h3>
+
+                <h6 className="avg__price">
+                  $13,605 <span>average price</span>
+                </h6>
+
+                <h6 className="market__price">Market average is $13,244</h6>
+                <span className="arrow__key">
+                  <i class="ri-arrow-right-line"></i>
+                </span>
+              </div>
+
+              <div className="circle__wrapper">
+                <div className="box__02">
+                  <CircularProgressbar
+                    value={percentage02}
+                    text={`${percentage02}%`}
                     styles={buildStyles({
                       pathColor: "#01d293",
                       textColor: "#fff",
